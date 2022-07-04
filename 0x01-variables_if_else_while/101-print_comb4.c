064a51237742fa3829abfec164a11f2490c90859
AmanuelTES <107487173+AmanuelTES@users.noreply.github.com>
2022-07-04 17:56:54 +0300

    co

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,24 +5,17 @@
 */
 int main(void)
 {
-int n, m, 1;
+int n, m;
 for (n = 48; n < 58; n++)
 {
-for (m = 49; m < 58; m++)
-{
-for (1 = 50; 1 < 58; 1++)
-{
-if (1 > m && m > n)
+for (m = 48; m < 57; m++)
 {
 putchar(n);
 putchar(m);
-putchar(1);
 if (n != 55 || m != 56)
 {
 putchar(',');
 putchar(' ');
-}
-}
 }
 }
 }
