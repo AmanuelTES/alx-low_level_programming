2d04150f7b51c05724a984960df783e86f5ce372
AmanuelTES <107487173+AmanuelTES@users.noreply.github.com>
2022-07-04 15:39:50 +0300

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,14 +4,8 @@
  * main - prints the phrase "programming is like building a multilingual puzzle"
  * Return : 0 if exited properly, non-zero otherwise
 */
-int main(void);
+int main(void)
 {
-    char ch;
-
-    puts(ch = "\"programming is like building a multilingual puzzel";ch);
-    {
-        putchar(ch)
-    }
-    putchar ('\n');
+    puts("\"programming is like building a multilingual puzzel");
     return (0);
 }
