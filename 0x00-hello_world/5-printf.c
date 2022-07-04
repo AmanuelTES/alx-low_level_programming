c774d134a51aa5bbfbb90fcb1d4469d33fba2751
AmanuelTES <107487173+AmanuelTES@users.noreply.github.com>
2022-07-04 15:43:32 +0300

    h

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  * main - prints the phrase "with proper grammer, but the outcomes is a piece of art"
  * Return : 0 if exited properly, non-zero otherwise
 */
-int main(void);
+int main(void)
 {
-    printf("with proper grammer, but the outcome is a piece of art, \n");
+    printf("with proper grammer, but the outcome is a piece of art,\n");
     return (0);
 }
