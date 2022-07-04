15ef95cd39db798a68619eb0103503fad9291215
AmanuelTES <107487173+AmanuelTES@users.noreply.github.com>
2022-07-04 17:48:58 +0300

    Update 102-print_comb5.c
    gg

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 * main -prints all possible combinations of two two digit numbers
-* Return : Always 0 (Success)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
@@ -22,8 +22,7 @@ if (i != 98 || j !=99)
 putchar(',');
 putchar(' ');
 }
-}
-}}
+}}}
 putchar('\n');
 return (0);
 }
