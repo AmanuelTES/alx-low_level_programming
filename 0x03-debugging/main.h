e5d5f9edf6a635cd772bee91f171d7c370e6c4bb
AmanuelTES <107487173+AmanuelTES@users.noreply.github.com>
2022-07-07 21:26:03 +0300

    sd

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,9 @@
 #include <stdio.h>
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+a = 972;
+b = -98;
+c = 0;
 int convert_day(int month, int day);
 void positive_or_negative(int i);
 #endif /* MAIN_H */
