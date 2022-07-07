bcdea2ef405494032872a88b77c62a9a329d72d7
AmanuelTES <107487173+AmanuelTES@users.noreply.github.com>
2022-07-07 23:59:59 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,11 +6,13 @@
 int main(void)
 {
 int i;
+printf("Infinite loop incoming :(\n");
 i = 0;
 while (i < 10)
 {
 putchar(i);
 }
 positive_or_negative(i);
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
