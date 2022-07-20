96819ce5707316952ff77168fd7a611bf21e5518
AmanuelTES <107487173+AmanuelTES@users.noreply.github.com>
2022-07-20 17:29:54 +0300

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,43 +1,24 @@
 #include "main.h"
-int bandersnatch(char *s1, char *s2);
-char *move(char *s2);
+/**
+* wildcmp - Funtion
+*
+* @s1: Variable
+* @s2: Variable
+*
+* Return: Int
+*/
 int wildcmp(char *s1, char *s2)
 {
-int sum = 0;
-if (*s1 == '\0' && *s2 == '*' && !*move(s2))
-return (1);
-if (*s1 == *s2)
-{
-if (*s1 == '\0')
-return (1);
+if (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
 return (wildcmp(s1 + 1, s2 + 1));
-}
-if (*s1 == '\0' || *s2 == '\0')
-return (0);
-if (*s2 == '*')
-{
-s2 = move(s2);
-if (*s2 == '\0')
+if (*s1 == '\0' && *s2 == '\0')
 return (1);
-if (*s1 == *s2)
-sum += wildcmp(s1 + 1, s2 + 1);
-sum += bandersnatch(s1 + 1, s2);
-return (!!sum);
-}
-return (0);
-}
-int bandersnatch(char *s1, char *s2)
-{
-if (*s1 == '\0')
-return (0);
-if (*s1 == *s2)
-return (wildcmp(s1, s2));
-return (bandersnatch(s1 + 1, s2));
-}
-char *move(char *s2)
-{
 if (*s2 == '*')
-return (move(s2 + 1));
-else
-return (s2);
+{
+if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+return (1);
+if (*(s1 + 1) != '\0' && *s2 == '\0')
+return (0);
+}
+return (0);
 }
