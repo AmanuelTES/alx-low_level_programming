a2f012859846b2099f2392cd8bfa230976d0612f
AmanuelTES <107487173+AmanuelTES@users.noreply.github.com>
2022-07-22 23:29:26 +0300

    be

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - prints all arguments it recieves
- * argc: number of arguments passed to the function
- * argv: argument vector of pointers to strings
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
  * Return: always 0
  */
 int main(int argc, char *argv[])
