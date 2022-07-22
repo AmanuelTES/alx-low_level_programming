226858e70811db7967965d3096de84c3020e56e7
AmanuelTES <107487173+AmanuelTES@users.noreply.github.com>
2022-07-22 23:23:26 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,17 +9,14 @@
 int main(int argc, char *argv[])
 {
 int a, b, c;
-
 if (argc != 3)
 {
 puts("Error");
 return (1);
 }
-
 a = atoi(argv[1]);
 b = atoi(argv[2]);
-c = a * b;
-
+c = a*b;
 printf("%d\n", c);
 return (0);
 }
