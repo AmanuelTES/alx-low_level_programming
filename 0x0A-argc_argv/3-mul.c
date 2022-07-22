31f315500e86dcc4c66428b2a96a72d93a7f854c
AmanuelTES <107487173+AmanuelTES@users.noreply.github.com>
2022-07-22 23:37:18 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - multiplies two numbers
  * @argc: number of arguments passed to the function
@@ -10,12 +9,13 @@
 int main(int argc, char *argv[])
 {
 int a, b, c;
-
 if (argc != 3)
+
 {
 puts("Error");
 return (1);
 }
+
 a = atoi(argv[1]);
 b = atoi(argv[2]);
 c = a * b;
