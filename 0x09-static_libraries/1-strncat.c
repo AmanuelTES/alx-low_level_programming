0f2d6c01535f164fab0bf2f7801a475e372b66c5
AmanuelTES <107487173+AmanuelTES@users.noreply.github.com>
2022-07-25 13:31:08 +0300

    asa

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strncat - concatenates n bytes of two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * @n: number of bytes
+ * Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+i = 0;
+while (dest[i] != '\0') /*WHILE count character */
+{
+i++;
+}
+j = 0;
+while (j < n && src[j] != '\0') /*WHILE concatenate dest with src*/
+{
+dest[i] = src[j];
+j++;  /*sum j*/
+i++; /**sum i*/
+}
+dest[i] = '\0'; /*the terminating null byte*/
+return (dest);
+}
