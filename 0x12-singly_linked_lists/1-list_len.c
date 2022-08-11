a58a16fc0c43e7cdcec647336f20e137097572bf
AmanuelTES <107487173+AmanuelTES@users.noreply.github.com>
2022-08-11 21:37:57 +0300

    asdsa

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,18 @@
+#include <stdlib.h>
 #include "lists.h"
 /**
- * list_len - the function that return the number of elements
- * in a linked list.
- * @h: the pointer that points to the struct.
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
  *
- * Return: number of elements
+ * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
 {
-int element;
-element = 0;
-while (h != NULL)
+size_t n = 0;
+while (h)
 {
+n++;
 h = h->next;
-element++;
 }
-return (element);
+return (n);
 }
--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,8 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <string.h>
-#include <stdio.h>
-#include <stddef.h>
 #include <stdlib.h>
 /**
  * struct list_s - singly linked list
@@ -18,7 +15,8 @@ typedef struct list_s
 char *str;
 unsigned int len;
 struct list_s *next;
-} list_t;
+}
+list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
